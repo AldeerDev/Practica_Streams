368871ae9abfc0df1b5b76611c563ebd5b89c34a
AldeerDev <aldeerdev@gmail.com>
2026-01-26 20:46:47 -0300

    Ejercicio 05: uso de count

---
+++ src/com/aldeerdev/practice/streams/StreamEjercicio05.java
@@ -0,0 +1,26 @@
+package com.aldeerdev.practice.streams;
+
+import java.util.List;
+
+public class StreamEjercicio05 {
+
+	public static void main(String[] args) {
+		/*
+		 * Ejercicio 05: count Contá cuántos nombres tienen más de 4 letras
+		 */
+
+		// Declaracion de la lista de nombres
+		List<String> nombres = List
+				.of("Bart", "Maggie", "Lisa", "Marge", "Homero");
+
+		// contar la cantidad de nombres con mas de 4 letras
+		Long cant = nombres
+				.stream()
+				.filter(nombre -> nombre.length() > 4)
+				.count();
+
+		// muestra la cantidad de nombres con mas de 4 letras por pantalla
+		System.out
+				.println("Cantidad de nombres con mas de 4 letras: " + cant);
+	}
+}
