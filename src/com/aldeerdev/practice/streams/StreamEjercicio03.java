56600f4289e8c3682b9f44521670daa0cf7a94ce
AldeerDev <aldeerdev@gmail.com>
2026-01-26 20:19:01 -0300

    Ejercicio 03: Uso de map

---
+++ src/com/aldeerdev/practice/streams/StreamEjercicio03.java
@@ -0,0 +1,19 @@
+package com.aldeerdev.practice.streams;
+
+import java.util.List;
+
+public class StreamEjercicio03 {
+
+	public static void main(String[] args) {
+		/*
+		 * Ejercicio 03: map Convertí todos los nombres a mayúsculas y mostralos
+		 */
+
+		// Declaracion de la lista de nombres
+		List<String> nombres = List.of("Bart", "Homero", "Lisa", "Marge", "Maggie");
+
+		// muestra los nombres de la lista en mayuscula
+		nombres.stream().map(String::toUpperCase).forEach(System.out::println);
+	}
+
+}
