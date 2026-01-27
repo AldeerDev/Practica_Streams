a602c48fe3cd810084aa1a3774c81a0e725eda8f
AldeerDev <aldeerdev@gmail.com>
2026-01-27 19:05:07 -0300

    Ejercicio 07: collectar con Set, eliminar duplicados de una lista

---
+++ src/com/aldeerdev/practice/streams/StreamEjercicio07.java
@@ -0,0 +1,28 @@
+package com.aldeerdev.practice.streams;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class StreamEjercicio07 {
+
+	public static void main(String[] args) {
+		/*
+		 * Ejercicio 07: collect a Set 1. Lista de nombres duplicados 2. Usar
+		 * Stream 3. Obener un Set sin duplicados 4. mostrarlo
+		 */
+
+		// Lista de nombres duplicados
+		List<String> nombresDuplicados = List.of("Homero", "Homero", "Marge",
+				"Marge", "Maggie");
+		
+		// Set de nombres
+		Set<String> nombresUnicos = nombresDuplicados.stream().collect(Collectors.toSet());
+		
+		// muestra los nombres unicos
+		System.out.println("Nombres unicos");
+		nombresUnicos.stream().forEach(System.out::println);
+
+	}
+
+}
