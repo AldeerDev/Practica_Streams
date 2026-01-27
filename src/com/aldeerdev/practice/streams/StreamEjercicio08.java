88d194082660f9d1e4076ad7152dfa2e50fd8d2b
AldeerDev <aldeerdev@gmail.com>
2026-01-27 19:32:55 -0300

    Ejercicio 08: Lista de tipo objeto

---
+++ src/com/aldeerdev/practice/streams/StreamEjercicio08.java
@@ -0,0 +1,30 @@
+package com.aldeerdev.practice.streams;
+
+import java.util.List;
+
+import com.aldeerdev.practice.model.Persona;
+
+public class StreamEjercicio08 {
+
+	public static void main(String[] args) {
+		/*
+		 * Ejercicio 08: Stream con objetos 
+		 * Consigna 
+		 * 1. Lista de personas 
+		 * 2. Filtrar mayores de 18 años 
+		 * 3. Mostrar nombre y edad
+		 */
+
+		// declaracion de la lista de tipo persona
+		List<Persona> personas = List.of(new Persona("Homero", 39),
+				new Persona("Marge", 38), new Persona("Bart", 10),
+				new Persona("Lisa", 8), new Persona("Maggie", 1),
+				new Persona("Ned", 60));
+
+		// filtra las personas mayores de 18 años y las muestra
+		personas.stream().filter(persona -> persona.getEdad() > 18)
+				.forEach(System.out::println);
+
+	}
+
+}
